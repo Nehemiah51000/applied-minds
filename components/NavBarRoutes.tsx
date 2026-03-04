17a27148d40f8f6ea163b0d15e885e4e4dd9a250
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2026-03-04 05:33:28 +0300

    Refactor: Removed unused state and effect hooks from NavBarRoutes component

--- components/NavBarRoutes.tsx
+++ components/NavBarRoutes.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { UserButton } from '@clerk/nextjs';
@@ -10,11 +9,6 @@ import { Button } from './ui/button';
 
 function NavBarRoutes() {
   const pathname = usePathname();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
 
   const isTeacherPage = pathname?.startsWith('/teacher');
   const isPlayerPage = pathname?.startsWith('/chapter');
@@ -35,7 +29,7 @@ function NavBarRoutes() {
           </Button>
         </Link>
       )}
-      {isMounted && <UserButton afterSignOutUrl='/' />}
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 }
