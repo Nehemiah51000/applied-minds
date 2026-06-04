a4365b30c4cddb4107b92e893e4e57f2680f47e4
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2026-06-04 13:46:34 +0300

    fix: update logo source path to 'AppliedMinds-logo.svg'

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 
 function Logo() {
-  return <Image height={130} width={130} alt='logo' src='/logo.svg' />;
+  return (
+    <Image height={130} width={130} alt='logo' src='/AppliedMinds-logo.svg' />
+  );
 }
 
 export default Logo;
